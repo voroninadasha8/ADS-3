2cbb55501302aeaee146b5bd7e52f07d8a286f51
voroninadasha8 <117026926+voroninadasha8@users.noreply.github.com>
2023-03-17 22:59:18 +0300

    Update and rename src/alg.cpp to Src/alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -2,5 +2,13 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
+  int res = 0;
+  int i = 0;
+  for (i; i <= size; i++) {
+    if (arr[i] == value) {
+      res += 1;
+    }
+    return res;
+  }
   return 0; // если ничего не найдено
 }
