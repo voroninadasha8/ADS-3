0e286bf25cd8054b547847b4583809b3a2ce1fa3
voroninadasha8 <117026926+voroninadasha8@users.noreply.github.com>
2023-03-29 21:57:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int start = 0;
+  int end = size - 1;
+  int count = 0;
+  while (start <= end) {
+    int mid = (start + end) / 2;
+    if (arr[mid] < value) {
+      start = mid + 1;
+    } else if (arr[mid] > value) {
+      end = mid - 1;
+    } else if (arr[mid] == value) {
+      count++;
+      int newmid = mid - 1;
+      while (arr[newmid] == value && newmid >= start) {
+        count++;
+        newmid--;
+      }
+      newmid = mid + 1;
+      while (arr[newmid] == value && newmid <= end) {
+        count++;
+        newmid++;
+      }
+      return count;
+    }
+  }
   return 0; // если ничего не найдено
 }
